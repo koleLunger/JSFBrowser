836141d33c3876457da8592221d0c37e868d452c
Christopher Smith <csmith@ytech.edu>
2019-11-21 20:34:48 -0500

    Functioning [v0.1.0]

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/', function(req, res, err) {
 });
 
 app.post('/search', function(req, res, err) {
-  request('http://localhost:3001/api/v0/search/' + req.body.search, function(request_err, request_res, request_body) {
+  request('http://localhost:3001/api/v0/fighter/search/' + req.body.search, function(request_err, request_res, request_body) {
     if (request_err || request_res.statusCode != 200) {
       res.send("Oops! There was a problem with the request module: <br>" + request_err);
     } else if (request_body == "undefined") {
@@ -37,7 +37,7 @@ app.post('/search', function(req, res, err) {
 });
 
 app.get('/list/fighters', function(req, res, err) {
-  request('http://localhost:3001/api/v0/list/all', function(request_err, request_res, request_body) {
+  request('http://localhost:3001/api/v0/fighter/list/', function(request_err, request_res, request_body) {
     if (request_err || request_res.statusCode != 200) {
       res.send("Oops! There was a problem with the request module: <br>" + request_err);
     } else if (request_body == "undefined") {
@@ -49,7 +49,7 @@ app.get('/list/fighters', function(req, res, err) {
 });
 
 app.get('/list/players', function(req, res, err) {
-  request('http://localhost:3001/api/v0/list/players', function(request_err, request_res, request_body) {
+  request('http://localhost:3001/api/v0/player/list', function(request_err, request_res, request_body) {
     if (request_err || request_res.statusCode != 200) {
       res.send("Oops! There was a problem with the request module: <br>" + request_err);
     } else if (request_body == "undefined") {
@@ -61,7 +61,7 @@ app.get('/list/players', function(req, res, err) {
 });
 
 app.get('/player/:player', function(req, res, err) {
-  request('http://localhost:3001/api/v0/player/' + req.params.player, function(request_err, request_res, request_body) {
+  request('http://localhost:3001/api/v0/player/show/' + req.params.player, function(request_err, request_res, request_body) {
     if (request_err || request_res.statusCode != 200) {
       res.send("Oops! There was a problem with the request module: <br>" + request_err);
     } else if (request_body == "undefined") {
@@ -72,12 +72,12 @@ app.get('/player/:player', function(req, res, err) {
   })
 });
 
-app.get('/fighter/:fighter', function(req, res, err) {
-  request('http://localhost:3001/api/v0/fighter/' + req.params.fighter, function(request_err, request_res, request_body) {
+app.get('/fighter/:fighterUID', function(req, res, err) {
+  request('http://localhost:3001/api/v0/fighter/show/' + req.params.fighterUID, function(request_err, request_res, request_body) {
     if (request_err || request_res.statusCode != 200) {
       res.send("Oops! There was a problem with the request module: <br>" + request_err);
     } else if (request_body == "undefined") {
-      res.send("Oops! Server didn't find that fighter in the database: <br>" + req.params.fighter);
+      res.send("Oops! Server didn't find that fighter in the database: <br>" + req.params.fighterUID);
     } else {
       res.status(200).render('fighter', JSON.parse(request_body));
     }
