fbc2441d7a820c12d760ee55fe70cdefd791f4b7
sfetrow <steph2021833@live.ytech.edu>
2019-11-25 10:17:36 -0500

    fixed requested changes

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.post('/search', function(req, res, err) {
 });
 
 app.get('/list/fighters', function(req, res, err) {
-    request('http://jsfdb.supermechacow.com/api/v0/fighter/list/views/listfighters.pug', function(request_err, request_res, request_body) {
+    request('http://jsfdb.supermechacow.com/api/v0/fighter/list/', function(request_err, request_res, request_body) {
         if (request_err || request_res.statusCode != 200) {
             res.send("Oops! There was a problem with the request module: <br>" + request_err);
         } else if (request_body == "undefined") {
